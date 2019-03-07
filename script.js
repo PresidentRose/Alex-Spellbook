04db7ab0acb560cf5e3c01da29e5c23fd69614e9
PresidentRose <43241735+PresidentRose@users.noreply.github.com>
2019-03-07 09:24:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".b1").click(function() {
+    $("h2, img").fadeToggle();
+});
+
+$(".b2").click(function() {
+    $("h2, img").slideToggle();
+});
+
+$(".b3").click(function() {
+    $("h2, img").toggle();
+});
